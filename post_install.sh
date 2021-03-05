c116539c9f97b109aae78f9ccc6857ba85bcfca9
MarcProux <marc.proux@outlook.fr>
2021-03-05 11:54:48 +0100

    First iteration

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,54 @@
 #!/bin/sh
-	
+
 # This script gets executed inside the jail right after plugin installation
 # This is executed BEFORE any jailman scripts
+
+# Constants --------------------------------------------------------------------
+CRONFILE="/tmp/cronfile"
+SYSCTLFILE="/etc/sysctl.conf"
+
+
+# Configure the cron -----------------------------------------------------------
+# Add a sleep to ensure that everything is ready before running script.
+cat <<EOF > ${CRONFILE}
+# One time run -----------------------------------------------------------------
+@reboot sleep 10; /WIREGUARD/COMMON/setup
+@reboot sleep 10; /WIREGUARD/FIREWALL/ipfw default
+
+# Continuously running ---------------------------------------------------------
+@reboot sleep 10; /WIREGUARD/APP/watch
+@reboot sleep 10; /WIREGUARD/WEB/watch
+EOF
+crontab -u root "${CRONFILE}"
+
+
+# Configure kernel -------------------------------------------------------------
+cat <<EOF >> ${SYSCTLFILE}
+net.inet.tcp.tso="0"
+net.inet.ip.forwarding="1"
+EOF
+
+
+# Configure services -----------------------------------------------------------
+# Firewall
+sysrc firewall_enable="YES"
+sysrc firewall_nat_enable="YES"
+sysrc firewall_script="/WIREGUARD/FIREWALL/ipfw default"
+sysrc firewall_logging="YES"
+
+# To allow NAT
+sysrc gateway_enable="YES"
+
+# WireGuard
+sysrc wireguard_enable="YES"
+sysrc wireguard_interfaces="/WIREGUARD/APP/wg0.conf"
+
+# WireGuard API
+sysrc wireguardapi_enable="YES"
+sysrc wireguardapi_device="wg0"
+sysrc wireguardapi_logging="YES"
+
+# WireGuard Web
+sysrc wireguardweb_enable="YES"
+sysrc wireguardweb_dir="/WIREGUARD/WEB"
+sysrc wireguardweb_logging="YES"
--- readme.md
+++ readme.md
@@ -1,13 +1,28 @@
-# {{PLUGIN_NAME}}
+# iocage-network-wireguard
 
 ## Installing Plugins
 Download the plugin manifest file to your local file system.
 
 ```bash
-fetch https://raw.githubusercontent.com/PiESTR-TrueNAS/{{PLUGIN_NAME}}/master/manifest.json
+fetch https://raw.githubusercontent.com/PiESTR-TrueNAS/iocage-network-wireguard/master/manifest.json
 ```
 
 Install the plugin. Adjust the network settings as needed.
 ```bash
 iocage fetch -P manifest.json
 ```
+
+
+## FAQ
+
+- **Extending functionality**
+> A mountpoint can be added to the jail as `/mnt/W`. This one will be populated by a [skeleton][1] on jail restart allowing you to change web endpoint environment and firewall rules.
+
+
+## Sources
+- https://sirtoffski.github.io/docs/freebsd-ipfw/
+- https://github.com/SirToffski/WireGuard-Ligase
+
+
+
+[1]:./overlay/WIREGUARD/COMMON/SKELETON
